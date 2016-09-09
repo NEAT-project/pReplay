0cfc686ed2cece05d0a38cab5f096ac65a5ffca9
moharaji <moharaji@kau.se>
2016-09-09 15:32:15 +0200

    filename processing is complete

--- experiment_scripts/process_json.sh
+++ experiment_scripts/process_json.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
+
+
 if [ ! -d temp_json ]; then
   mkdir -p temp_json;
 fi
 rm temp_json/*.json
 
-for file in *.har ;
+for file in *.json ;
 	do
 	fname=$file
 	SFI=$IFS; IFS='_';
@@ -19,11 +23,6 @@ for file in *.har ;
 	#echo $rtt $plr $csz $nc $ptl $count
 	IFS=$SFI 
 	set +f;
-	#printf '{"RTT":%s,"PLR":%s,"CSZ":%s, "NC":%s, "PROTOCOL": "%s", "COUNT": %s}\n' "$rtt" "$plr" "$csz" \
-#		"$nc" "$ptl" "$count" > temp.har
-	#jq -s '.' $fname temp.har
-	#jq -c '.del(.OLT)' $fname
-#	echo $fname
         OUT=$(mktemp temp_json/XXXX.json);
 	jq -c 'del(.OLT)' $fname > temp && mv temp $OUT
 	jq  --argjson rtt_ ${rtt}   '.RTT=$rtt_' $OUT > temp && mv temp $OUT
@@ -32,24 +31,6 @@ for file in *.har ;
 	jq  --argjson nc_ ${nc}   '.NC=$nc_' $OUT > temp && mv temp $OUT
 	jq  --arg ptl_ ${ptl}   '.PROTOCOL=$ptl_' $OUT > temp && mv temp $OUT
 	jq  --argjson cnt_ ${count}   '.COUNT=$cnt_' $OUT > temp && mv temp $OUT
-#       cat $OUT
-#	#result="$rslt$(cat $file|sed 's/],//g')";
-#        #OUT=$(mktemp temp_json/XXXX.json);
-#	#echo $result > $OUT
 done
 
-for i in {20,80,194}; do
-	for j in {0,0.03};do
-		for k in {0,512,2048}; do
-			for l in {1,6,18}; do
-				for m in {"http","phttpget"}; do
-					#awk -v a="$i" -v b="$j" -v c="$k" -v d="$l" -v e="$m" '{if ($4==a && $5==b && $6=c && $7==d && $8==e){sum+=$10;num+=1; print "--"$10}}END{print sum/num}' tmp.rslt
-					awk -v a="$i" -v b="$j" -v c="$k" -v d="$l" -v e="$m" '{if ($4==a && $5==b && $6=c && $7==d && $8==e){print $0}}' tmp.rslt
-
-						
-
-				done
-			done
-		done
-	done	
-done 
+python json_reader.py > tmp_results
